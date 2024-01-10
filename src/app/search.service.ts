7f70ef4f5ad044c9caccddaba4ebaeb039831fae
Pingo <kokatic@gmail.com>
2024-01-10 11:17:36 +0100

    Update

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -9,14 +9,15 @@ import { Observable } from 'rxjs';
 })
 export class SearchService {
   private rapidApiKey = '6a41403a8dmsh8898c2340dbfb77p1e19a1jsn77590d3f64f5';
-  private rapidApiBaseUrl = 'https://kokatic.azurewebsites.net/geonames/';
-
+  private rapidApiBaseUrl = 'https://getcity4.p.rapidapi.com/geonames/';
+  private proxy='af29a400-a335-11ee-8149-cf495a77a1b8'
   constructor(private http: HttpClient) {}
 
   private getHeaders(): HttpHeaders {
     return new HttpHeaders({
       'X-RapidAPI-Host': 'getcity4.p.rapidapi.com',
-      'X-RapidAPI-Key': this.rapidApiKey
+      'X-RapidAPI-Key': this.rapidApiKey,
+      'X-RapidAPI-Proxy-Secret': this.proxy
     });
   }
 
