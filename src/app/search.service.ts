13e0e630698f979fba6343f0d0d721549a8a14c3
Pingo <kokatic@gmail.com>
2024-01-10 14:43:58 +0100

    Update

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,15 +1,14 @@
-// src/app/search.service.ts
-
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
   private rapidApiKey = '6a41403a8dmsh8898c2340dbfb77p1e19a1jsn77590d3f64f5';
-  private rapidApiBaseUrl = 'https://getcity4.p.rapidapi.com/geonames';
+  private rapidApiBaseUrl = 'https://getcity4.p.rapidapi.com/geonames/';
   private proxy='af29a400-a335-11ee-8149-cf495a77a1b8'
   constructor(private http: HttpClient) {}
 
@@ -28,6 +27,7 @@ export class SearchService {
     const params = new HttpParams()
     .set('city_name', cityName)
     .set('country_code', countryCode);
+    // Include headers in the HTTP request
     return this.http.get(apiUrl, { headers, params });
   }
 }
